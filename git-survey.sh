8055bc5a5fcdf27e763a8293db882fd658c4163d
wyattscarpenter <wyattscarpenter@gmail.com>
2024-08-29 23:56:50 -0700

    refactor header code in git-survey

--- git-survey.sh
+++ git-survey.sh
@@ -1,23 +1,23 @@
+header () {
+  echo -e "\n\e[7m[$@]\e[0m"
+}
+
 underline_quine () {
   echo -e "\e[4m$@\e[0m"
   $@
 }
-echo -e "\e[7m[STATUS]\e[0m"
+header STATUS
   git status
-echo
-echo -e "\e[7m[LOGHEAD]\e[0m"
+header LOGHEAD
   git log -n 1
-echo
-echo -e "\e[7m[IDENTITY & CREDENTIAL CONFIGS]\e[0m"
+header IDENTITY & CREDENTIAL CONFIGS
   underline_quine git whoami
   underline_quine git config credential.helper
   underline_quine git config credential.useHttpPath
-echo
-echo -e "\e[7m[ORIGIN]\e[0m"
+header ORIGIN
   git remote show origin | grep "URL\|up to date\|local out of date"
   git ls-remote #the point of this command is to show what the most recent commit is on the remote, but also log you in if you aren't already.
-echo
-echo -e "\e[7m[NO""CHECKINS]\e[0m"
+header "NO""CHECKINS"
   export LC_ALL=C #this is just a funny hack to get grep to go faster. I'm not sure it actually does anything for the git-internal grep.
   git "no""checkins"
 echo
