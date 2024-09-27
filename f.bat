1af3c969c82a9c043da1fad73edd6d4198a91393
wyattscarpenter <wyattscarpenter@gmail.com>
2024-09-26 20:50:05 -0700

    rearrange f.bat to be more tolerant of old git-repl versions

--- f.bat
+++ f.bat
@@ -1,2 +1,2 @@
 REM this uses the repl command from Git Extras https://github.com/tj/git-extras , with the passing of a command into git repl being available in versions 7.3.0 and later. (You might need to install directly from github, considering how far behind most distros are on this.)
-cmd /K git repl fsg
+cmd /K "git fsg && git repl"
